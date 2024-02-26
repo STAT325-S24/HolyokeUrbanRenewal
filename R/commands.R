3a149d7a40862b3923baa614c062b57a2b1d06c1
Nicholas Horton <nicholasjhorton@gmail.com>
2024-02-26 18:06:41 -0500

    Minor tweaks: ready to share with students!

--- R/commands.R
+++ R/commands.R
@@ -6,12 +6,6 @@
 #' XX
 #' 
 #' @param formula the formula describing the relationship
-#' @param data a data frame (or you can pipe this in)
-#' @param ... statistics functions to be applied to the data, e.g. mean, sd, confidence(0.95)
-#' @param as.percent show proportions in percent (e.g. multiply by 100)
-#' @param wide reformat the output as a cross-tabulation. This makes sense only when there are just two variables
-#' @param margins show the marginal probabilities. Makes the most sense if \code{wide = TRUE}.
-#' @param format Use just for internal purposes.
 #' 
 #' @examples
 #' get_filename()
