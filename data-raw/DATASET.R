550ae48dfd6c4271f2b9b4cd50f2b34f8a008b80
Nicholas Horton <nicholasjhorton@gmail.com>
2024-02-27 10:37:25 -0500

    Updating data and DESCRIPTION.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 HolyokeUrbanRenewal <-
-  readxl::read_xlsx("data-raw/riverview_renewal_data_2023_05_08.xlsx") |>
+  readxl::read_xlsx("data-raw/riverview_renewal_data_2024_02_27.xlsx") |>
   janitor::clean_names()
-usethis::use_data(HolyokeUrbanRenewal)
+usethis::use_data(HolyokeUrbanRenewal, overwrite = TRUE)
