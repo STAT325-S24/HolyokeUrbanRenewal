9bfec0a29bfaa388a1306f7e9144df6898aebe28
jpapagelis24 <jpapagelis24@amherst.edu>
2024-02-27 15:53:49 -0500

    Steps wrangling the data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 HolyokeUrbanRenewal <-
   readxl::read_xlsx("data-raw/riverview_renewal_data_2024_02_27.xlsx") |>
-  janitor::clean_names()
+  janitor::clean_names() |>
+  mutate(parcel = str_replace(parcel, "\\.0$", ""),
+         deed_reference_book = str_replace(deed_reference_book, "\\.0$", ""),
+         deed_reference_page = str_replace(deed_reference_page, "\\.0$", ""),
+         assessed_value = str_extract_all(assessed_value, "\\$.*"))
 usethis::use_data(HolyokeUrbanRenewal, overwrite = TRUE)
