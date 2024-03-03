692f4768ed4507c6bd42bbb6df5c59a3d0bf9c2c
Nicholas Horton <nicholasjhorton@gmail.com>
2024-03-03 08:53:57 -0500

    I still don't really understand images in Shiny. But this fix seems to have worked. See https://github.com/STAT325-S24/HolyokeUrbanRenewal/issues/12

--- shiny/app.R
+++ shiny/app.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(png) # For writePNG function
+# library(png) # For writePNG function
 library(shiny)
 library(stringr)
 # usethis::install_github("STAT325-S24/HolyokeUrbanRenewal")
@@ -15,17 +15,19 @@ image_path <- paste0(
   system.file(package = "HolyokeUrbanRenewal"),
   "/extdata"
 )
+
 all_buildings <- HolyokeUrbanRenewal::get_buildings()
 files <- all_buildings$filename
 
 unique_buildings <- all_buildings |>
   select(block, parcel) |>
+  mutate(property_label = paste0("Block ", block, ", Parcel ", parcel)) |>
   unique()
 
 
 clean_files <- files[str_detect(files, "\\.jpeg")]  # check for correct format
 # not clear that this next line is needed (or what it is testing for)
-file_names <- lapply(clean_files, get_Filename, pattern = "[.]") |> unlist()
+# file_names <- lapply(clean_files, get_Filename, pattern = "[.]") |> unlist()
 
 ui <-
   navbarPage("Holyoke Riverview Prospect Heights Urban Renewal project",
@@ -34,7 +36,10 @@ ui <-
       mainPanel(
         p("This app displays information about the Holyoke Riverview Prospect Heights Urban Renewal project."),
         p("This project would not have been made possible without the efforts of Eileen Crosby (Holyoke Public Library History Room)."),
-        uiOutput("tab")
+        uiOutput("tab"),
+        wellPanel(
+          radioButtons("building", "Select a building:", unique_buildings$property_label, selected = unique_buildings$property_label[1])
+        )
     )),
 
     tabPanel(
@@ -45,11 +50,11 @@ ui <-
         column(
           3, 
           wellPanel(
-            radioButtons("picture", "Picture:", file_names)
+            uiOutput("building_images"),
           )
         ),
         column(
-          6,
+          7,
           textOutput("bldg_info"),
           imageOutput("image")
         )
@@ -69,13 +74,17 @@ ui <-
 server <- function(input, output, session) {
   
   output$bldg_info <- renderText({
-    return(paste0("XX summary goes here! XX This is ", input$picture))
+    return(paste0("Displaying information for ", input$building))
   })
   
   output$image <- renderImage({
-    width  <- session$clientData$output_image_width
-    height <- session$clientData$output_image_height
-    index <- which(file_names == input$picture)
+    # width  <- session$clientData$output_image_width
+    #cat("width: ", width, "\n")
+    width <- 630
+    #height <- session$clientData$output_image_height
+    #cat("height: ", height, "\n")
+    height <- 840
+    index <- which(clean_files == input$picture)
     src <- paste0(image_path, "/", files[index])
     results <- file.exists(src)
     if (!results) {
@@ -90,16 +99,27 @@ server <- function(input, output, session) {
     ))
   }, deleteFile = FALSE)
   
-  url <- 
-    a("For more information click here", href="https://github.com/STAT210-S23/STAT210-Holyoke-History-Room/tree/main/Riverview_Renewal_Project")
+  url <- a(
+    "For more information click here", 
+    href="https://github.com/STAT210-S23/STAT210-Holyoke-History-Room/tree/main/Riverview_Renewal_Project"
+  )
   output$tab <- renderUI({
     tagList("Link to scanned images:", url)
+  })
+  
+  output$building_images <- renderUI({
+    this_building <- unique_buildings |>
+      filter(property_label == input$building)
+    my_parcel <- this_building |> pull(parcel)
+    my_block <- this_building |> pull(block)
+    building_files <- all_buildings |>
+      filter(parcel == my_parcel, block == my_block)
+    radioButtons("picture", "Select an image:", building_files$filename, selected = building_files$filename[1])
   })
   
   output$table <- DT::renderDT(
     HolyokeUrbanRenewal::HolyokeUrbanRenewal |>
       select(-file_name))
-  
 }
 
 
